823a2848b107ed4bc487fdbfccc14eaa2d6582d5
coconhat <nhat_vu_le@dlsl.edu.ph>
2024-09-11 07:33:05 +0800

    added temp header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-      
+      <Header />
     </>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <Logo />
+      <p className="letterHeader">APOD NASA NEWS </p>
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div>
+      <img src="src/assets/temp-logo.png" alt="" className="logoImage"/>
+    </div>
+  );
+}
+
+function SearchBar(){
+  return (
+    <div>
+      
+    </div>
   )
 }
 
-export default App
+export default App;
