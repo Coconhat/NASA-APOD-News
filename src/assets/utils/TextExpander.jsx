b125b6c3baf6d6987c1b28eee270adc398570bf9
coconhat <nhat_vu_le@dlsl.edu.ph>
2024-10-23 16:57:50 +0800

    added news up to 15 days and fix minor bugs

--- src/assets/utils/TextExpander.jsx
+++ src/assets/utils/TextExpander.jsx
@@ -1,26 +1,22 @@
-import React, { Children, useState } from "react";
-import Button from "../../components/Button";
+import React, { useState } from "react";
+import Button from "../../components/Button"; // Ensure this is the correct path
 
 export default function TextExpander({
   children,
   numberOfText = 10,
-  expandedText = "show less",
-  notExpandedText = "read more",
+  expandedText = "Show less",
+  notExpandedText = "Read more",
 }) {
   const [isExpand, setIsExpand] = useState(false);
 
   return (
     <div>
-      
-        {isExpand ? (
-          <p>{children} </p>
-        ) : (
-          <p>{children.split(" ").slice(0, numberOfText).join(" ") + "..."}</p>
-        )}{" "}
-        <Button onClick={() => setIsExpand(!isExpand)}>
-          {isExpand ? expandedText : notExpandedText}
-        </Button>
-      
+      {isExpand ? (
+        <p>{children}</p>
+      ) : (
+        <p>{children.split(" ").slice(0, numberOfText).join(" ") + "..."}</p>
+      )}
+     
     </div>
   );
 }
